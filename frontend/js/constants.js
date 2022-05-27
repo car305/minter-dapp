0612535b10e028a1e27966892b1b40eb5add073d
car305 <sherwincarreos@gmail.com>
2022-05-27 09:13:36 +0800

    added contract address

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xC1cEdC7DBf49E3dFCF47800c5789851ceEC1DeD3"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";

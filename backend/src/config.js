5dda80a04afbcf32c6a72345ca5aeb30414cd029
car305 <sherwincarreos@gmail.com>
2022-05-27 19:26:46 +0800

    updated config information

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,24 +7,286 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "PHILIPPINE HEROES";
+const description = "Let's give tribute to our heroes who fought for our freedom and independence";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
-  {
-    growEditionSizeTo: 5,
+  //gear only
+  // {
+  //   growEditionSizeTo: 1000,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Chest" },
+  //     { name: "Eyeshield" },
+  //     { name: "Helmet" },
+  //     { name: "Shoulders" },
+  //     { name: "Mask" },
+  //   ],
+  // },
+  //    {
+  //   growEditionSizeTo: 1,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Abad Santos Face" },
+  //     { name: "Base Shade M" },
+  //     { name: "Shirt" },
+  //     { name: "Abad Santos Eyes" },
+  //     { name: "Abad Santos Nose" },
+  //     { name: "Abad Santos Mouth" },
+  //     { name: "Abad Santos Eyebrow" },
+  //     { name: "Abad Santos Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 2,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Aguinaldo Face" },
+  //     { name: "Shirt" },
+  //     { name: "Aguinaldo Eyes" },
+  //     { name: "Aguinaldo Nose" },
+  //     { name: "Aguinaldo Mouth" },
+  //     { name: "Aguinaldo Eyebrow" },
+  //     { name: "Aguinaldo Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 3,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade F" },
+  //     { name: "Aquino Face" },
+  //     { name: "Shirt" },
+  //     { name: "Aquino Eyes" },
+  //     { name: "Aquino Nose" },
+  //     { name: "Aquino Mouth" },
+  //     { name: "Aquino Eyebrow" },
+  //     { name: "Aquino Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 4,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Bonifacio Face" },
+  //     { name: "Shirt" },
+  //     { name: "Bonifacio Eyes" },
+  //     { name: "Bonifacio Nose" },
+  //     { name: "Bonifacio Mouth" },
+  //     { name: "Bonifacio Eyebrow" },
+  //     { name: "Bonifacio Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 5,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Burgos Face" },
+  //     { name: "Shirt" },
+  //     { name: "Burgos Eyes" },
+  //     { name: "Burgos Nose" },
+  //     { name: "Burgos Mouth" },
+  //     { name: "Burgos Eyebrow" },
+  //     { name: "Burgos Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 6,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Del Pilar Face" },
+  //     { name: "Shirt" },
+  //     { name: "Del Pilar Eyes" },
+  //     { name: "Del Pilar Nose" },
+  //     { name: "Del Pilar Mouth" },
+  //     { name: "Del Pilar Eyebrow" },
+  //     { name: "Del Pilar Beard" },
+  //     // { name: "Del Pilar Facial Hair" },
+  //     { name: "Del Pilar Moustache" },
+  //     { name: "Del Pilar Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 7,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade F" },
+  //     { name: "Escoda Face" },
+  //     { name: "Shirt" },
+  //     { name: "Escoda Eyes" },
+  //     { name: "Escoda Nose" },
+  //     { name: "Escoda Mouth" },
+  //     { name: "Escoda Eyebrow" },
+  //     { name: "Escoda Hair" },
+  //   ],
+  // }, 
+  // {
+    //   growEditionSizeTo: 8,
+    //   layersOrder: [
+      //     // { name: "Background" },
+      //     { name: "Base" },
+      //     { name: "Base Shade M" },
+  //     { name: "Gomez Face" },
+  //     { name: "Shirt" },
+  //     { name: "Gomez Eyes" },
+  //     { name: "Gomez Nose" },
+  //     { name: "Gomez Mouth" },
+  //     { name: "Gomez Eyebrow" },
+  //     { name: "Gomez Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 9,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Jacinto Face" },
+  //     { name: "Shirt" },
+  //     { name: "Jacinto Eyes" },
+  //     { name: "Jacinto Nose" },
+  //     { name: "Jacinto Mouth" },
+  //     { name: "Jacinto Eyebrow" },
+  //     { name: "Jacinto Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 10,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Lapulapu Face" },
+  //     { name: "Lapulapu Skin" },
+  //     { name: "Base Shade M" },
+  //     { name: "Shirt" },
+  //     { name: "Lapulapu Eyes" },
+  //     { name: "Lapulapu Nose" },
+  //     { name: "Lapulapu Mouth" },
+  //     { name: "Lapulapu Eyebrow" },
+  //     { name: "Lapulapu Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 11,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Luna Face" },
+  //     { name: "Shirt" },
+  //     { name: "Luna Eyes" },
+  //     { name: "Luna Nose" },
+  //     { name: "Luna Mouth" },
+  //     { name: "Luna Eyebrow" },
+  //     { name: "Luna Facial Hair" },
+  //     { name: "Luna Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 12,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Mabini Face" },
+  //     { name: "Shirt" },
+  //     { name: "Mabini Eyes" },
+  //     { name: "Mabini Nose" },
+  //     { name: "Mabini Mouth" },
+  //     { name: "Mabini Eyebrow" },
+  //     { name: "Mabini Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 13,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Rizal Face" },
+  //     { name: "Shirt" },
+  //     { name: "Rizal Eyes" },
+  //     { name: "Rizal Nose" },
+  //     { name: "Rizal Mouth" },
+  //     { name: "Rizal Eyebrow" },
+  //     { name: "Rizal Facial Hair" },
+  //     { name: "Rizal Hair" },
+  //   ],
+  // },
+  // {
+  //   growEditionSizeTo: 14,
+  //   layersOrder: [
+  //     // { name: "Background" },
+  //     { name: "Base" },
+  //     { name: "Base Shade M" },
+  //     { name: "Zamora Face" },
+  //     { name: "Shirt" },
+  //     { name: "Zamora Eyes" },
+  //     { name: "Zamora Nose" },
+  //     { name: "Zamora Mouth" },
+  //     { name: "Zamora Eyebrow" },
+  //     { name: "Zamora Hair" },
+  //   ],
+  // },
+ {
+    growEditionSizeTo: 10,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+    //   // { name: "Background" },
+      { name: "Base" },
+      { name: "Face" },
+      { name: "Skin" },
+      { name: "Base Shade M" },
+      { name: "Shirt" },
+      { name: "Eyes M" },
+      { name: "Nose" },
+      { name: "Mouth" },
+      { name: "Eyebrow" },
+      { name: "Mole" },
+      { name: "Beard" },
+      { name: "Moustache" },
+      { name: "Hair M" },
+      // { name: "Chest" },
+      // { name: "Eyeshield" },
+      // { name: "Helmet" },
+      // { name: "Shoulders" },
+      // { name: "Mask" },
     ],
   },
+//   { 
+//     growEditionSizeTo: 1000,
+//     layersOrder: [
+//       // { name: "Background" },
+//       { name: "Base" },
+//       { name: "Face" },
+//       { name: "Skin" },
+//       { name: "Base Shade F" },
+//       { name: "Shirt" },
+//       { name: "Eyes F" },
+//       { name: "Nose" },
+//       { name: "Mouth" },
+//       { name: "Eyebrow" },
+//       { name: "Mole" },
+//       { name: "Hair F" },
+//       // { name: "Chest" },
+//       // { name: "Eyeshield" },
+//       // { name: "Helmet" },
+//       // { name: "Shoulders" },
+//       // { name: "Mask" },
+//     ],
+//   },
 ];
 
 const shuffleLayerConfigurations = true;
@@ -32,13 +294,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 3000,
+  height: 3900,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://bangonpilipinas.com", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,34 +311,37 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'PHILIPPINE HEROES';
+const CONTRACT_SYMBOL = 'PHL';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xBE0317DA7bf17F660DFFA487a12254A8baD94503';
+const TREASURY_ADDRESS = '0xBE0317DA7bf17F660DFFA487a12254A8baD94503';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-08-29T10:00:48+00:00"; // FINAL date : 8/29/2022 10:00am uses GMT
+// const PUBLIC_MINT_START_DATE = "2022-05-26T03:00:00+00:00"; // Test date uses GMT
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+// const PRESALE_MINT_START_DATE = "2022-08-22T02:00:00+00:00"; // FINAL date : 8/22/2022 10:00am uses GMT
+const PRESALE_MINT_START_DATE = "2022-05-26T02:00:00+00:00"; // Test date uses GMT
+const ROYALTY_SHARE = 200; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xBE0317DA7bf17F660DFFA487a12254A8baD94503"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x3c9D2B371C44b8Ea5ef66655f63F46521cf7D960"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+// let CONTRACT_ADDRESS = "0x85414D3dE74ad5e7b7DE08Bf5F92bD69570C6C99"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xC1cEdC7DBf49E3dFCF47800c5789851ceEC1DeD3"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Let's give tribute to our heroes who fought for our freedom and independence. Which hero will you get?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeiezlbb4ahkj6ec4fm76lvc6n2bbgqxndrpl3jgl3osq5wy5lxpmou"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -125,11 +390,11 @@ const text = {
 };
 
 const pixelFormat = {
-  ratio: 2 / 128,
+  ratio: 18 / 128,
 };
 
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
   static: false,
   default: "#000000",
